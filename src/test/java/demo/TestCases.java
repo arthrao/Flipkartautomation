53ce649d2618e12ba999c1762ccee9b1275db90f
Arth Rao <arths9900@gmail.com>
2024-12-22 14:01:47 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
-
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
@@ -22,17 +21,17 @@ public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,18 +45,84 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
-
+        driver.get("http://www.flipkart.com/");
         driver.manage().window().maximize();
+
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
     }
+
+    @Test(description = "Search Product by Popularity")
+    public void testCase01() {
+        System.out.println("Start TestCase001");
+
+        Wrappers wrapper = new Wrappers(driver);
+
+        
+        
+
+        By searchproduct = By.xpath("//input[@name='q']");
+        By click = By.xpath("//button[@type='submit']");
+        wrapper.searchProduct(searchproduct, click, "Washing Machine");
+
+        By filter = By.xpath("//div[contains(text(),'Popularity')]");
+        wrapper.sortingpopulairty(filter);
+
+        By ratings = By.xpath("//div[@class='XQDdHH']");
+        wrapper.ratingcalculator(ratings);
+
+        System.out.println("End TestCase001");
+
+    }
+
+    @Test(description = "Search IPhone Product")
+    public void testCase02() {
+        System.out.println("Start TestCase002");
+
+        Wrappers wrapper = new Wrappers(driver);
+        By searchproduct = By.xpath("//input[@name='q']");
+        By click = By.xpath("//button[@type='submit']");
+        wrapper.searchProduct(searchproduct, click, "iPhone");
+
+        int discountlimit = 17;
+        By phonetitle = By.xpath("//div[@class='KzDlHZ']");
+        By discounttitle = By.xpath("//div[@class='UkUFwK']");
+        wrapper.printDiscountedItems(phonetitle, discounttitle, discountlimit);
+        System.out.println("End TestCase002");
+
+    }
+
+    @Test(description = "Search Coffee Mug")
+    public void testCase03() {
+        System.out.println("Start TestCase003");
+
+        Wrappers wrapper = new Wrappers(driver);
+        By searchproduct = By.xpath("//input[@name='q']");
+        By click = By.xpath("//button[@type='submit']");
+        wrapper.searchProduct(searchproduct, click, "Coffee Mug"); 
+
+        By ratingfilter = By.xpath("(//div[@class='XqNaEv'])[1]");
+        wrapper.applyfilter(ratingfilter);
+
+        
+
+        By title = By.xpath("//a[@class='wjcEIp']");
+        By review = By.xpath("//span[@class='Wphh3N']");
+        
+        By image = By.xpath("//img[@loading='eager']");
+        
+
+        wrapper.extractAndPrintDetails(title, review, image);
+        
+        System.out.println("End TestCase003");
+    }
+
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -2,15 +2,18 @@ package demo.wrappers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -19,4 +22,216 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    
+    public void WrapperClass(String driverPath) {
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void launchWebsite(String url) {
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+
+    public void searchProduct(By searchlocator,By buttonlocator,String product){
+        try {
+            
+        
+        WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(searchlocator));
+        Thread.sleep(500);
+        searchInput.sendKeys(Keys.CONTROL + "a"); 
+        searchInput.sendKeys(Keys.BACK_SPACE);
+        //searchInput.clear();
+        Thread.sleep(3000);
+        searchInput.sendKeys(product);
+        Thread.sleep(2000);
+        WebElement search = wait.until(ExpectedConditions.visibilityOfElementLocated(buttonlocator));
+        search.click();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+        
+    }
+
+    public void sortingpopulairty(By locator){
+        try {
+            
+       Thread.sleep(2000);
+        WebElement filtertext = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        filtertext.click();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+    public void ratingcalculator(By locator){
+        try {
+            
+        Thread.sleep(3000);
+        List<WebElement> ratingElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+        int count = 0;
+
+        for (WebElement ratingElement : ratingElements) {
+            String ratingText = ratingElement.getText();
+            if (!ratingText.isEmpty()) {
+                try {
+                    double rating = Double.parseDouble(ratingText);
+                    if (rating <= 4.0) {
+                        count++;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid rating format: " + ratingText);
+                }
+            }
+        }
+        System.out.println("Count of items with rating <= 4 stars: " + count);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+    public void printDiscountedItems(By phonelocator, By discountLocator, int threshold) {
+        try {
+            
+            Thread.sleep(3000);
+        List<WebElement> titles = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(phonelocator));
+        List<WebElement> discounts = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(discountLocator));
+    
+        System.out.println("Items with more than " + threshold + "% discount:");
+    
+        for (int i = 0; i < titles.size() && i < discounts.size(); i++) {
+            String title = titles.get(i).getText();
+            String discountText = discounts.get(i).getText().replaceAll("[^0-9]", "");
+    
+            try {
+                int discount = Integer.parseInt(discountText);
+                if (discount > threshold) {
+                    System.out.println("Title: " + title + " | Discount: " + discount + "%");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid discount format for item: " + title);
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    }
+
+    public void applyfilter(By locator){
+        try {
+            WebElement ratingfilter = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            
+            ratingfilter.click();
+            System.out.println("Filter applied successfully.");
+
+        } catch (Exception e) {
+            System.err.println("Error while applying filter: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    public void extractAndPrintDetails(By titleLocator, By reviewLocator, By imageLocator) {
+        try {
+            Thread.sleep(3000); 
+
+            
+            List<WebElement> titleElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(titleLocator));
+            List<WebElement> reviewElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(reviewLocator));
+            List<WebElement> imageElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(imageLocator));
+
+            List<Item> items = new ArrayList<>();
+
+            for (int i = 0; i < titleElements.size(); i++) {
+                try {
+                    String title = titleElements.get(i).getText();
+                    String reviews = reviewElements.get(i).getText();
+                    String imageUrl = imageElements.get(i).getAttribute("src");
+
+                    int reviewCount = parseReviews(reviews);
+
+                    items.add(new Item(title, reviewCount, imageUrl));
+                } catch (Exception e) {
+                    System.out.println("Error extracting item details at index " + i + ": " + e.getMessage());
+                }
+            }
+
+            
+            List<Item> topItems = items.stream()
+                    .sorted(Comparator.comparingInt(Item::getReviews).reversed())
+                    .limit(5)
+                    .collect(Collectors.toList());
+
+            
+            System.out.println("Top 5 Coffee Mug Items with Highest Reviews:");
+            for (Item item : topItems) {
+                System.out.println("Title: " + item.getTitle());
+                System.out.println("Image URL: " + item.getImageUrl());
+                System.out.println("Reviews: " + item.getReviews());
+                
+            }
+            Thread.sleep(2000);
+
+        } catch (Exception e) {
+            System.out.println("Error during extraction and printing: " + e.getMessage());
+        }
+    }
+
+    
+    private int parseReviews(String reviewsText) {
+        try {
+            return Integer.parseInt(reviewsText.replaceAll("[^0-9]", ""));
+        } catch (NumberFormatException e) {
+            return 0; 
+        }
+    }
+
+    
+    private static class Item {
+        private String title;
+        private int reviews;
+        private String imageUrl;
+
+        public Item(String title, int reviews, String imageUrl) {
+            this.title = title;
+            this.reviews = reviews;
+            this.imageUrl = imageUrl;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public int getReviews() {
+            return reviews;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+    }
+
+    
+
 }
+
+
+    
+
+    
+
+
+
+    
+
